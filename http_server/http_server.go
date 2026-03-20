67fc660f570980720ed01aea4696a438d68bfdaa
Andrew <asoborev@mail.ru>
2026-03-20 23:15:38 +0300

    write docker file

--- http_server/http_server.go
+++ http_server/http_server.go
@@ -10,7 +10,7 @@ func Start() error {
 	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("processed request /ping")
 		
-		w.Write([]byte("hello from Docker"))
+		w.Write([]byte("hello from Docker\n"))
 	})
 
 	err := http.ListenAndServe(":5050", nil)
