48d85fa2719b8bd9d9522c4bf1a9900bf6f6cd62
Andrew <asoborev@mail.ru>
2026-03-24 23:04:31 +0300

    оптимизация dockerfile

--- main.go
+++ main.go
@@ -1,31 +1,32 @@
 package main
 
 import (
-	"os"
+	"docker-http-server/http_server"
+	"fmt"
 )
 
 func main() {
-	// fmt.Println("Server start")
+	fmt.Println("Server start")
 
-	// err := http_server.Start()
+	err := http_server.Start()
+	if err != nil {
+		fmt.Println("Во время работы с сервером произошла ошибка:", err)
+		return
+	} else {
+		fmt.Println("Сервер завершился успешно")
+	}
+
+	// err := os.MkdirAll("out", 0755)
 	// if err != nil {
-	// 	fmt.Println("Во время работы с сервером произошла ошибка:", err)
-	// 	return
-	// } else {
-	// 	fmt.Println("Сервер завершился успешно")
+	// 	panic(err)
 	// }
 
-	err := os.MkdirAll("out", 0755)
-	if err != nil {
-		panic(err)
-	}
+	// file, err := os.Create("out/test.txt")
+	// if err != nil {
+	// 	panic(err)
+	// }
 
-	file, err := os.Create("out/test.txt")
-	if err != nil {
-		panic(err)
-	}
+	// file.Write([]byte("my test file"))
 
-	file.Write([]byte("my test file"))
-
-	defer file.Close()
+	// defer file.Close()
 }
